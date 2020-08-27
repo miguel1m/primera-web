45f93b20d757b3c5200986747ad3d4c672901570
miguel1m <martinezmoreno.miguel@gmail.com>
2020-08-27 13:31:44 +0200

    formulario de contacto y contact.php

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+$para = 'martinezmoreno.miguel@gmail.com';
+$asunto = "Mensaje desde pagina web";
+
+$mailheader = "From: " .$_POST["email"]."\r\n";
+$mailheader .= "Reply-To: " .$_POST["email"]."\r\n";
+$mailheader .= "Content-type: text/html; charset=utf-8\r\n";
+
+$MESSAGE_BODY = "Nombre: " .$_POST["name"]."\r\n";
+$MESSAGE_BODY .= "\n<br>Email: " .$_POST["email"]."\r\n";
+$MESSAGE_BODY .= "\n<br>Mensaje " .$_POST["mensaje"]."\r\n";
+
+mail($para, $asunto, $MESSAGE_BODY, $mailheader) or die("Error al enviar el mail");
+
+header('Location: pagina donde se redirige al enviar mail');
+
+
+?>
